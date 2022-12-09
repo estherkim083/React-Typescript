266e93a1bb4cbe9512532951cbd1b2b0ee77a9db
estherkim083 <estherkim083@gmail.com>
2022-12-09 13:12:07 +0900

    refs

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,15 @@
 import ReactDOM from "react-dom/client";
 //import EventComponent from "./events/EventComponent";
-import UserSearch from "./classes/UserSearch";
+import UserSearch from "./refs/UserSearch";
 
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el!);
-const users = [
-  { name: "John", age: 28 },
-  { name: "Alex", age: 28 },
-  { name: "Michael", age: 28 },
-];
 
 const App = () => {
   return (
     <div>
-      <UserSearch users={users} />
+      <UserSearch />
     </div>
   );
 };
